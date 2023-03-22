0ded4004e7dc0192491aabaa25c7a59742842621
Jose Aguilar <joshxyev@gmail.com>
2023-03-23 00:04:37 +0100

    feat: :sparkles: add check task error handler

--- src/supabase/task/setTaskStatus.ts
+++ src/supabase/task/setTaskStatus.ts
@@ -1,17 +1,17 @@
-import type { PostgrestSingleResponse } from '@supabase/supabase-js'
-
 import supabase from '../client'
 
-const setTaskStatus = async (
-  id: number,
-  status: boolean
-): Promise<PostgrestSingleResponse<null>> => {
+const setTaskStatus = async (id: number, status: boolean): Promise<never> => {
   const res = await supabase
     .from('todos')
     .update({ is_complete: status })
     .eq('id', id)
+    .single()
 
-  return res
+  if (res.error != null) {
+    throw new Error()
+  }
+
+  return res.data
 }
 
 export default setTaskStatus
